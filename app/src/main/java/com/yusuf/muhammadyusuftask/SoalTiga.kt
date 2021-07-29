77bc9d775149451ba12d0458d819c4c3de9b85ba
Muhammad Yusuf <muhammad.yusuf18010@student.unsika.ac.id>
2021-07-29 12:51:44 +0700

    adding number 3

--- app/src/main/java/com/yusuf/muhammadyusuftask/SoalTiga.kt
+++ app/src/main/java/com/yusuf/muhammadyusuftask/SoalTiga.kt
@@ -2,10 +2,44 @@ package com.yusuf.muhammadyusuftask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import com.yusuf.muhammadyusuftask.databinding.ActivitySoalSatuBinding
+import com.yusuf.muhammadyusuftask.databinding.ActivitySoalTigaBinding
 
 class SoalTiga : AppCompatActivity() {
+    private lateinit var binding: ActivitySoalTigaBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_soal_tiga)
+        binding = ActivitySoalTigaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        binding.btnProses.setOnClickListener {
+            val radioGroup: RadioGroup = binding.formatwaktu
+            val jam = binding.inptJam.text.toString()
+            val menit = binding.inptMenit.text.toString()
+            val detik = binding.inptDetik.text.toString()
+            val format: Int = radioGroup.checkedRadioButtonId
+
+            if(format!= -1 ){
+                var radio : RadioButton = findViewById(format)
+                if(jam.toInt() <= 12){
+                    if(radio.text.toString() == "AM"){
+                        binding.hasil.text = jam +":"+ menit +":"+ detik
+                    }else if(radio.text.toString() == "PM"){
+                        var konversi = jam.toInt() + 12
+                        if(konversi == 24){
+                            binding.hasil.text = "00" +":"+ menit +":"+ detik
+                        }else {
+                            binding.hasil.text = konversi.toString() + ":" + menit + ":" + detik
+                        }
+                    }
+                }else{
+                    binding.hasil.text = "jam error"
+                }
+            }
+        }
     }
 }
